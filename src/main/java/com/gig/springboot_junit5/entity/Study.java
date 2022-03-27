7197d80b4aa8d421223fd7f7ced3f380eb0c8656
kjuiop <copying_y@naver.com>
2022-03-27 13:02:52 +0900

    junit test repeat

--- src/main/java/com/gig/springboot_junit5/entity/Study.java
+++ src/main/java/com/gig/springboot_junit5/entity/Study.java
@@ -11,17 +11,44 @@ import lombok.NoArgsConstructor;
  */
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Study {
 
     private StudyStatus studyStatus = StudyStatus.DRAFT;
 
     private int limit;
 
+    private String name;
+
     public Study(int limit) {
         if (limit < 0) {
             throw new IllegalArgumentException("limit는 0보다 커야 한다.");
         }
         this.limit = limit;
+    }
+
+    public Study(Integer limit) {
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit는 0보다 커야 한다.");
+        }
+        this.limit = limit;
+    }
+
+    public Study(StudyStatus studyStatus, int limit) {
+        this.studyStatus = studyStatus;
+        this.limit = limit;
+    }
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "studyStatus=" + studyStatus +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                "}";
     }
 }
--- src/test/java/com/gig/springboot_junit5/StudyTest.java
+++ src/test/java/com/gig/springboot_junit5/StudyTest.java
@@ -5,6 +5,16 @@ import com.gig.springboot_junit5.entity.type.StudyStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -23,7 +33,71 @@ import static org.junit.jupiter.api.Assumptions.assumingThat;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
+    @DisplayName("반복 테스트")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}, {totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
 
+    @DisplayName("파라미터 반복 테스트")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    @EmptySource
+    @NullSource
+    @NullAndEmptySource
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
+
+    @DisplayName("파라미터 반복 테스트 Csv")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTestConvertWith(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    @DisplayName("파라미터 반복 테스트 Csv")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTestCsvSource(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study);
+    }
+
+    @DisplayName("파라미터 반복 테스트 ArgumentAccessor")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTestArgumentAccessor(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println(study);
+    }
+
+    @DisplayName("파라미터 반복 테스트 AggregatorWith")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTestAggregatorWith(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        // static inner class 나 public class 로 만들어야 사용할 수 있다.
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
+    }
 
     @Test
     @DisplayName("조건에 따라 실행되는 테스트")
